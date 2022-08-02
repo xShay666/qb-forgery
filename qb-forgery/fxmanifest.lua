bf9021afe4fdaa84cb7076b9d60b7f21853a4e50
xShay <107325067+xShay666@users.noreply.github.com>
2022-08-02 08:21:42 +0300

    Add files via upload

---
+++ qb-forgery/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Created By xShay'
+description 'An ID/Drivers License forging system.'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
